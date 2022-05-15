2ab78c1a58c356838b4fb1e1658cca71709091e9
Roduan Kareem Aldeen <roduan98@gmail.com>
2022-05-15 18:41:48 +0300

    move closures to controllers

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function index()
+    {
+        $messages = Message::all();
+
+        return view('messages.index', compact('messages'));
+    }
+
+    public function show (Message $message)
+    {
+        return view('messages.show', compact('message'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|min:3|max:255',
+            'email' => 'required|email',
+            'phone' => 'required|starts_with:9639|digits_between:12,12',
+            'content' => 'required|string|min:5',
+        ]);
+
+        $message = new Message();
+        $message->name = $request->name;
+        $message->email = $request->email;
+        $message->phone = $request->phone;
+        $message->content = $request->content;
+        $message->save();
+
+        return redirect('/#contact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Models\Message;
-use Illuminate\Http\Request;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,38 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
+Route::get('/', [HomeController::class, 'welcome'])->name('home');
 
 Route::view('/about', 'pages.about')->name('about');
 Route::view('/contact-us', 'pages.contact')->name('contact');
 
-Route::post('/contact-us', function (Request $request) {
-    $request->validate([
-        'name' => 'required|string|min:3|max:255',
-        'email' => 'required|email',
-        'phone' => 'required|starts_with:9639|digits_between:12,12',
-        'content' => 'required|string|min:5',
-    ]);
-
-    $message = new Message();
-    $message->name = $request->name;
-    $message->email = $request->email;
-    $message->phone = $request->phone;
-    $message->content = $request->content;
-    $message->save();
-
-    return redirect('/#contact');
-});
-
-Route::get('/admin/messages', function () {
-    $messages = Message::all();
-
-    return view('messages.index', compact('messages'));
-})->name('messages.store');
-
-Route::get('/admin/messages/{message}', function (Message $message) {
-    return view('messages.show', compact('message'));
-})->name('messages.show');
+Route::post('/contact-us', [MessageController::class, 'store'])->name('messages.store');
+Route::get('/admin/messages', [MessageController::class, 'index'])->name('messages.index');
+Route::get('/admin/messages/{message}', [MessageController::class, 'show'])->name('messages.show');
