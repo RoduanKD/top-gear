75b9fea2e4be27a20ba93e8a4b57f8a3f2e37d1e
Roduan Kareem Aldeen <roduan98@gmail.com>
2022-05-20 15:40:04 +0300

    add categoy index and create

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|min:4|max:255',
+            'capacity' => 'required|numeric|min:2',
+        ]);
+
+        $category = Category::create($validated);
+
+        return redirect()->route('categories.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        // $category->update($validated);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\CarController;
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
@@ -23,14 +24,16 @@ Route::view('/contact-us', 'pages.contact')->name('contact');
 
 Route::post('/contact-us', [MessageController::class, 'store'])->name('messages.store');
 
+Route::prefix('admin')->group(function () {
+    Route::get('messages', [MessageController::class, 'index'])->name('messages.index');
+    Route::get('messages/{message}', [MessageController::class, 'show'])->name('messages.show');
 
-Route::get('/admin/messages', [MessageController::class, 'index'])->name('messages.index');
-Route::get('/admin/messages/{message}', [MessageController::class, 'show'])->name('messages.show');
-
-Route::get('/admin/cars', [CarController::class, 'index'])->name('cars.index');
-Route::get('/admin/cars/create', [CarController::class, 'create'])->name('cars.create');
-Route::get('/admin/cars/{car}', [CarController::class, 'show'])->name('cars.show');
-Route::post('/admin/cars', [CarController::class, 'store'])->name('cars.store');
-Route::get('/admin/cars/{car}/edit', [CarController::class, 'edit'])->name('cars.edit');
-Route::put('/admin/cars/{car}', [CarController::class, 'update'])->name('cars.update');
-Route::delete('/admin/cars/{car}', [CarController::class, 'destroy'])->name('cars.destroy');
+    Route::get('cars', [CarController::class, 'index'])->name('cars.index');
+    Route::get('cars/create', [CarController::class, 'create'])->name('cars.create');
+    Route::get('cars/{car}', [CarController::class, 'show'])->name('cars.show');
+    Route::post('cars', [CarController::class, 'store'])->name('cars.store');
+    Route::get('cars/{car}/edit', [CarController::class, 'edit'])->name('cars.edit');
+    Route::put('cars/{car}', [CarController::class, 'update'])->name('cars.update');
+    Route::delete('cars/{car}', [CarController::class, 'destroy'])->name('cars.destroy');
+    Route::resource('categories', CategoryController::class);
+});
