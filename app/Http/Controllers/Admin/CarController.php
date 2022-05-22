91ed8ce4c60187bdb0e8863bf83529e8364b289c
tamara <tamshehadeh@gmail.com>
2022-05-22 19:42:08 +0300

    add admin to car show roure

--- app/Http/Controllers/Admin/CarController.php
+++ app/Http/Controllers/Admin/CarController.php
@@ -73,7 +73,7 @@ class CarController extends Controller
      */
     public function show(Car $car)
     {
-        return view('cars.show', compact('car'));
+        return view('admin.cars.show', compact('car'));
     }
 
     /**
