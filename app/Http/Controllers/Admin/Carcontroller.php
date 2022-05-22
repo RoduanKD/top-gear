120b4e2d0040259180046c91d1165a885897102e
tamara <tamshehadeh@gmail.com>
2022-05-22 14:26:38 +0300

    edit car,category controllers

--- app/Http/Controllers/Admin/Carcontroller.php
+++ app/Http/Controllers/Admin/Carcontroller.php
@@ -73,7 +73,8 @@ class CarController extends Controller
      */
     public function show(Car $car)
     {
-        return view('cars.show', compact('car'));
+
+        return view('admin.cars.show', compact('car'));
     }
 
     /**
