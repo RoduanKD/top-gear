8e077bd1d687d61d092c84ee215814786242f9d2
tamara <tamshehadeh@gmail.com>
2022-05-22 13:33:23 +0300

    edit all cars views

--- resources/views/admin/cars/index.blade.php
+++ resources/views/admin/cars/index.blade.php
@@ -1,33 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($cars as $car)
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">{{ $car->brand }} {{ $car->model }}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
-                    <form action="{{ route('cars.destroy', $car) }}" method="POST"> @csrf @method('DELETE') <button type="submit">Delete</button> </form>
-                {{-- <a href="{{ route('cars.destroy', $car) }}" class="card-link" @method('DELETE')>Delete</a> --}}
-                {{-- <a href="{{ route('cars.edit', $car) }}" class="card-link" @method('DELETE')>Delete</a> --}}
-                <form action="{{ route('cars.edit', $car) }}" method="PUT"> @csrf <button type="submit">Edit</button> </form>
-            </div>
+    <div class="container my-5">
+        <div class="full">
+            <h1>Here's Your Cars !</h1>
         </div>
 
+        <div class="row">
+            @foreach ($cars as $car)
+                <div class="col-md-4">
 
-        {{-- <a href="{{ route('cars.destroy', $car) }}" class="card-link">Delete</a>
-    <a href="#" class="card-link">Another link</a> --}}
+                    <div class="card cardhov my-2" style="width: 18rem;">
+                        <img class="card-img-top"
+                            src="https://d.newsweek.com/en/full/1949339/tesla-model-s.jpg?w=1600&h=900&q=88&f=e5d09ec2030e76aba072a36c90568476"
+                            alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $car->brand }} {{ $car->model }}</h5>
+                            <p class="card-text">{{ $car->description }}</p>
 
+                                <a href="{{ route('cars.show', $car) }}"  class="text-primary stretched-link"> show more info</a>
+
+                                <div class="row my-2">
+                                <div class="col">
+                                    <form action="{{ route('cars.edit', $car) }}" method="PUT"> @csrf <button
+                                            class="btn ma-2" type="submit"
+                                            style="background-color: #161C34; color:white;">Edit</button> </form>
+                                </div>
+                                <div class="col">
+                                    <form action="{{ route('cars.destroy', $car) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE') <button style="background-color: #F36B2A; color:white;"
+                                            class="btn mb-2" type="submit" data-toggle="modal"
+                                            data-target="#exampleModal">Delete</button> </form>
+                                    {{-- <button type="button" class="btn" data-toggle="modal"
+                                            data-target="#exampleModal"
+                                            style="background-color: #F36B2A; color:white;" >
+                                             DELETE
+                                        </button>
+                                          <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal" tabindex="-1"
+                                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel">DELETE THIS CAR
+                                                        </h5>
+                                                        <button type="button" class="close" data-dismiss="modal"
+                                                            aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        ARE YOU SURE YOU WANT TO DELETE THIS CAR!
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-dismiss="modal">Close</button>
+                                                        <form action="{{ route('cars.destroy', $car) }}" method="POST">
+                                                            @csrf
+                                                            @method('DELETE') <button
+                                                                style="background-color: #F36B2A; color:white;"
+                                                                class="btn mb-2" type="submit" data-toggle="modal"
+                                                                data-target="#exampleModal">Delete</button> </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div> --}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            @endforeach
         </div>
-    @endforeach
-{{-- <div class="row">
-    @foreach($cars as $car)
-    <div class="col-md-4">
-        <a href="{{ route('cars.edit', $car) }}">
-            <h4>{{ $car->brand }} {{ $car->model }}</h4>
-            <h5><form action="{{ route('cars.destroy', $car) }}" method="POST"> @csrf @method('DELETE') <button type="submit">Delete</button> </form></h5>
-        </a>
     </div>
-    @endforeach --}}
-{{-- </div> --}}
 @endsection
+
+<style scoped>
+    .card:hover {
+        border-radius: 0.75rem;
+        border-color: #161C34;
+        transition-delay: 0.1s
+    }
+
+    h1 {
+        color: #161C34;
+    }
+
+</style>
