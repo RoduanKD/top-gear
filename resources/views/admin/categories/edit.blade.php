c9101e579da2193523eea3153f1c664b6d2e31d4
Silver-Bullet-11 <78919756+GhassanMg@users.noreply.github.com>
2022-05-25 07:08:14 +0300

    add missing required pages

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('title', 'Add a new category')
+
+@section('content')
+    <section class="section py-10" style="padding-bottom: 50px">
+        <div class="container">
+            <form action="{{ route('categories.update', $category) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
+                        placeholder="Economy" value="{{ $category->name }}">
+                    @error('name')
+                        <div class="invalid-feedback">{{ $category }}</div>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="capacity">Capacity</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">person</span>
+                        </div>
+                        <input name="capacity" id="capacity" type="number" min="2"
+                            class="form-control @error('capacity') is-invalid @enderror" value="{{ $category->capacity }}">
+                    </div>
+                    @error('capacity')
+                        <div class="invalid-feedback">{{ $category }}</div>
+                    @enderror
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </section>
+@endsection
