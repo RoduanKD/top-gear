772407c2edfb75637eaf6f2d4296e9f9f7518b0b
Mohammad Ghassan Al-Moghrabe <78919756+GhassanMg@users.noreply.github.com>
2022-06-02 09:46:39 +0300

    ad color controller after conflicts

---
+++ app/Http/Controllers/Admin/ColorController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Color;
+use Illuminate\Http\Request;
+
+class ColorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //dd('this is index function');
+        $colors = Color::all();
+
+        return view('admin.colors.index', compact('colors'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //dd('this is create function');
+        return view('admin.colors.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+           'name'  => 'required|min:3|max:12|string',
+        ]);
+
+        $color = Color::create($validated);
+
+        session()->flash('message', 'The color was added successfully');
+        session()->flash('message-type', 'success');
+
+        return redirect()->route('admin.colors.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Color  $color
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Color $color)
+    {
+        dd('this is show function');
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Color  $color
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Color $color)
+    {
+        dd('this is edit function');
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Color  $color
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Color $color)
+    {
+        dd('this is update function');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Color  $color
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Color $color)
+    {
+        dd('this is destroy function');
+
+    }
+}
